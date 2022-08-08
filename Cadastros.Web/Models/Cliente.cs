0b46d08f5cf0ae328f701dd694faba3363ed4f18
Gustavo Miranda <gustmrg@gmail.com>
2022-08-08 20:20:58 -0300

    Implementado o CRUD de clientes e contatos. Melhorado o layout das Views.

--- Cadastros.Web/Models/Cliente.cs
+++ Cadastros.Web/Models/Cliente.cs
@@ -6,16 +6,17 @@ public class Cliente
 {
     public int Id { get; set; }
     
-    [Required, StringLength(100)]
+    [Required, StringLength(100), Display(Name = "Nome Completo")]
     public string NomeCompleto { get; set; }
     
     [Required, StringLength(40)]
+    [EmailAddress]
     public string Email { get; set; }
     
     [Required, StringLength(13)]
     public string Telefone { get; set; }
     
-    [DataType(DataType.Date)]
+    [DataType(DataType.Date), Display(Name = "Data de Cadastro")]
     public DateTime DataCadastro { get; set; }
 
     public List<Contato> Contatos { get; set; } = new();
--- Cadastros.Web/Models/Contato.cs
+++ Cadastros.Web/Models/Contato.cs
@@ -6,10 +6,11 @@ public class Contato
 {
     public int Id { get; set; }
     
-    [Required, StringLength(100)]
+    [Required, StringLength(100), Display(Name = "Nome Completo")]
     public string NomeCompleto { get; set; }
     
     [Required, StringLength(40)]
+    [EmailAddress]
     public string Email { get; set; }
     
     [Required, StringLength(13)]
